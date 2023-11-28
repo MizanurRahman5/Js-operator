2ad48d357c6fb87d1678ddf9cf202cb99154214a
Misty Islam <134092022+MistyIslam5@users.noreply.github.com>
2023-11-28 23:58:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// var num1 = 10;
+//  var nume2 = 15;
+//  console.log(num1>nume2);  //output Fals
+
+
+
+
+
+// logical And operator
+
+
+// var num1 = 50;
+// var num2 = 30;
+// var num3 = 20;
+// console.log(num1>num2 && num1>num3);  //Output True
+
+
+// logical Or Operator
+var num1 = 50;
+var num2 = 30;
+var num3 = 20;
+
+ console.log(num1>num2 || num3>num2); //output true
+
